3462bdbfb0adb8cd3be50efb506a102ccd53e64f
ckcr4lyf <poiasdpoiasd@live.com>
2024-08-19 23:31:58 +0800

    Remove comment from source and move 3rd party notice to README

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,3 @@
-// DHT Temperature & Humidity Sensor
-// Unified Sensor Library Example
-// Written by Tony DiCola for Adafruit Industries
-// Released under an MIT license.
-
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
